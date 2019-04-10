75db493739928bc8ced2be62bafefccb0cb76d43
lbianculli <35279143+lbianculli@users.noreply.github.com>
2019-04-10 13:07:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,37 @@ from pysc2.env import sc2_env
 from pysc2.lib import actions
 from pysc2.env import environment
 
-import deepq_mineral_shards
+import mineral_shards_agent
 
 
-def main():
+def main(unusedargv):
+  # started to figure out whats going on here but a lot to chew on
+  agent = TerranAgent()
+  map = 'Simple64'
+  try:
+      while True:  
+          with sc2_env.SC2Env(
+              map_name=map,
+              players=[sc2_env.Agent(sc2_env.Race.terran),
+                       sc2_env.Bot(sc2_env.Race.terran,
+                                   sc2_env.Difficulty.very_easy)],
+              agent_interface_format=features.AgentInterfaceFormat( 
+                  feature_dimensions=features.Dimensions(screen=84, minimap=64),
+                  use_feature_units=True), 
+              step_mul=25, 
+              game_steps_per_episode=0, 
+              visualize=True  # True gives you a full version of the visuals
+              ) as env:
+
+              agent.setup(env.observation_spec(), env.action_spec())
+              timesteps = env.reset()
+              agent.reset()
+
+              while True:
+                  step_actions = [agent.step(timesteps[0])]
+                  if timesteps[0].last():  
+                      break
+                  timesteps = env.step(step_actions) 
+
+  
+  
