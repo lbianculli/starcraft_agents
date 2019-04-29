51b8c8a01cd1b4fb25fa6f1b6dd5b91b8d737bf3
lbianculli <35279143+lbianculli@users.noreply.github.com>
2019-04-29 13:24:39 -0400

    Update deepq_terran.py

--- deepq_terran.py
+++ deepq_terran.py
@@ -19,5 +19,28 @@ from baselines.deepq.utils import ObservationInput
 from baselines.common.tf_util import get_session
 from baselines.deepq.models import build_q_func
 
+# steps (for DQN): create env, create model, act = .learn (this is essentially the agent step), setup callback
+# actor critic requires the subproc for env.
+# the above steps are for [mineral shards] minigames -- what about regular game?
+
+
+class TerranAgent(base_agent.BaseAgent):
+	def __init__(self, ):
+
+
+
 def main():
+	with sc2_env.SC2Env(
+		map_name='Simple64', 
+		step_mul=8,
+		visualize=True,
+		screen_size_px=(64,64),
+		minimap_size_px=(64,64)) as env:
+						
+    
+  
+  
+  
+  
+  
 
