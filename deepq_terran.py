291f0fe56ea02a0a7a0bdd75d2e84cd99712b9ab
lbianculli <35279143+lbianculli@users.noreply.github.com>
2019-04-29 12:58:06 -0400

    Create deepq_terran.py

---
+++ deepq_terran.py
@@ -0,0 +1,23 @@
+import os
+import tempfile
+
+import tensorflow as tf
+import zipfile
+import cloudpickle
+import numpy as np
+
+import baselines.common.tf_util as U
+from baselines.common.tf_util import load_variables, save_variables
+from baselines import logger
+from baselines.common.schedules import LinearSchedule
+from baselines.common import set_global_seeds
+
+from baselines import deepq
+from baselines.deepq.replay_buffer import ReplayBuffer, PrioritizedReplayBuffer
+from baselines.deepq.utils import ObservationInput
+
+from baselines.common.tf_util import get_session
+from baselines.deepq.models import build_q_func
+
+def main():
+
