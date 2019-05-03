196b141ac63cfc4a86f9b5da3a956fdd7892930a
lbianculli <35279143+lbianculli@users.noreply.github.com>
2019-05-02 21:05:20 -0400

    Update deepq_terran2.py

--- deepq_terran2.py
+++ deepq_terran2.py
@@ -1,11 +1,9 @@
-# COPY THIS SOMEWHERE BEFORE RUNNING
-
 import numpy as np
 import os
 import dill
 import tempfile
 import tensorflow as tf
-from absl import app, flags
+from absl import app
 import pickle
 import pandas as pd
 import logging
@@ -14,14 +12,22 @@ import random
 import zipfile
 
 import baselines.common.tf_util as U
+import baselines.deepq.utils as U2
 from baselines import logger, deepq
 from baselines.common.schedules import LinearSchedule
 from baselines.deepq.replay_buffer import ReplayBuffer, PrioritizedReplayBuffer
 from baselines.deepq.models import build_q_func
 from baselines.deepq.utils import ObservationInput
+from deepq_inputs import BatchInput
+
+from pysc2.agents import base_agent
+from pysc2.env import sc2_env
+from pysc2.lib import actions, features, units
 
 
 EPISODE_REWARDS = [0.0]
+
+
 
 class ActWrapper(object):
     """ Takes batches of observations and returns actions  """
@@ -71,7 +77,7 @@ class ActWrapper(object):
 
 
 
-def load(path, act_params, num_cpu=4):  
+def load(path, act_params, num_cpu=4):
   """Load act function that was returned by learn function.
   Parameters
   ----------
@@ -93,19 +99,19 @@ def load(path, act_params, num_cpu=4):
 
 class TerranAgent(base_agent.BaseAgent):
     def __init__(self, # which init does this stuff go into?
-                 num_cpu=1,                      
+                 num_cpu=1,
                  max_timesteps=100000,
                  buffer_size=50000,
                  lr=5e-4,
-                 gamma=1.0, 
-                 scope=None, 
-                 prioritized_replay=False,
+                 gamma=1.0,
+                 scope=None,
+                 prioritized_replay=True,
                  prioritized_replay_alpha=0.6,
                  prioritized_replay_beta0=0.4,
                  prioritized_replay_beta_iters=None,
-                 prioritized_replay_eps=1e-6): 
-        
-        super(TerranAgent, self).__init__(self)
+                 prioritized_replay_eps=1e-6):
+
+        super(TerranAgent, self).__init__()
         self.num_cpu = num_cpu
         self.max_timesteps = max_timesteps
         self.buffer_size = buffer_size
@@ -123,50 +129,43 @@ class TerranAgent(base_agent.BaseAgent):
         self.action_y = None
         self.total_reward = 0
         self.step_count = 0
-        
-        
-    def setup(self, obs_spec, action_spec):
-        self.obs_spec = obs_spec
-        self.action_spec = action_spec
-#         self.act_x_wrapped, self.act_y_wrapped = self._setup_learn() # setup here i think, how exactly?
 
 
-    def reset(self):
-        self.episodes += 1
-
-        
     def step(self, obs, env):
-        done = obs[0].step_type == environment.StepType.LAST
-#         screen = (obs.observation.feature_minimap.player_relative == features.PlayerRelative.NEUTRAL).astype(int)
+        # print(obs.step_type)
+        if self.step_count == 0:  # how to account for last step?
+            print('ASDASDASDASDASDSDASD')
+            self._setup(len(obs.observation.available_actions))
 
-        self.replay_buffer_x.add(self.prev_obs, self.action_x, self.reward, obs, float(done))  # do i want screen or obs? 
-        self.replay_buffer_y.add(self.prev_obs, self.action_y, self.reward, obs, float(done))  
+#         screen = (obs.observation.feature_minimap.player_relative == features.PlayerRelative.NEUTRAL).astype(int)
+        self.replay_buffer_x.add(self.prev_obs, self.action_x, self.reward, obs, float(done))  # do i want screen or obs?
+        self.replay_buffer_y.add(self.prev_obs, self.action_y, self.reward, obs, float(done))
 #         self.replay_buffer_x.add(screen, action_x, step_reward, new_screen, float(done))
 #         self.replay_buffer_y.add(screen, action_y, step_reward, new_screen, float(done))
-        
+
         self.action_x, self.action_y = self.learn(env=env)  # what do these end up looking like? anything else needed?
         step_reward = obs[0].reward
-        self.prev_obs = obs  
+        self.prev_obs = obs
 
         EPISODE_REWARDS[-1] += step_reward  # supposed to append to episode_rewards if done. best way?
-        self.total_reward = EPISODE_REWARDS[-1]  
-        
+        self.total_reward = EPISODE_REWARDS[-1]
+
         return actions.FUNCTIONS.no_op()  # make sure runs w/ no_op() before confirming actions
-    
-        
-    def make_obs_ph(self, name):
-        return U.BatchInput((16, 16), name=name) 
-    
-    
-    def _setup_learn(self, num_actions): # how to determine? len(available_actions) prob. should prob be somewhere else
+
+
+    def make_obs_ph(self, name):  ### HERE ###
+          U2.ObservationInput(Box(low=-32.0, high=32.0, shape=(2,)), name=name)
+
+
+    def _setup(self, num_actions): # how to determine? len(available_actions) prob. should prob be somewhere else
         '''
         initializes session, x/y vars, replay_buffer, beta_schedule, exploration, and target networks
         returns: actwrappers for x and y. SHOULD BE CALLED ONLY ONCE. num actions changes though....
         '''
-        
-        self.sess = U.make_session(self.num_cpu)
+
+        self.sess = U.make_session(num_cpu=self.num_cpu)
         self.sess.__enter__()
-        
+
         self.act_x, self.train_x, self.update_target_x, self.debug_x = deepq.build_train(
             make_obs_ph = self.make_obs_ph,
             q_func=self.model,
@@ -221,17 +220,17 @@ class TerranAgent(base_agent.BaseAgent):
 
 #         self.episode_rewards = [0.0]  # do i need/
         self.saved_mean_reward = None
-        
+
         ### print test ###
         print(f'ActWrapper around act_x: {ActWrapper(act_x)}')
         print(f'act_x: {act_x}')
         return ActWrapper(act_x), ActWrapper(act_y) # do i need the wrapper even?
-        
-        
-    def learn(self, 
+
+
+    def learn(self,
               env,  # will env being a requirement cause any trouble here? could i simply just use within main()
-              q_func=self.q_func,
-              num_actions=4,  # ?
+              q_func,
+              num_actions,  # ?
               exploration_fraction=.01,
               exploration_final_eps=0.02,
               train_freq=1,
@@ -252,10 +251,6 @@ class TerranAgent(base_agent.BaseAgent):
         updating exploration value, picking x/y actions, sampling from and updating buffer,
         updating target networks, and logging.
         '''
-        
-        if callback is not None:
-            if callback(locals(), globals()):
-                break
         kwargs = {}
         if not param_noise:  # variance reduction stuff (?) could go out of order and check this lecture next
             update_eps = self.exploration.value(self.step_count)
@@ -270,7 +265,7 @@ class TerranAgent(base_agent.BaseAgent):
                 # See Appendix C.1 in Parameter Space Noise for Exploration, Plappert et al., 2017 for more detail
                 update_param_noise_threshold = -np.log(
                     1. - self.exploration.value(self.step_count) + self.exploration.value(self.step_count) / float(num_actions))
-        
+
         # not sure what this is about
         kwargs['reset'] = reset
         kwargs['update_param_noise_threshold'] = update_param_noise_threshold
@@ -278,21 +273,21 @@ class TerranAgent(base_agent.BaseAgent):
 
         # THIS is where it needs to go down, how to choose an actions? Dont I need categorical and spatial actions?
         # the [None] inserts a dimension along axis=0
-        # would really help if i knew what act_x/y looked like -- from obs to obs  
-        action_x = self.act_x(np.array(screen)[None], update_eps=update_eps, **kwargs)[self.step_count]  #*** this is the bottleneck 
+        # would really help if i knew what act_x/y looked like -- from obs to obs
+        action_x = self.act_x(np.array(screen)[None], update_eps=update_eps, **kwargs)[self.step_count]  #*** this is the bottleneck
         action_y = self.act_y(np.array(screen)[None], update_eps=update_eps, **kwargs)[self.step_count]  # return this?
         reset = False
 #         coord = [player[0], player[1]]
         coord = [action_x, action_y]  #  why two coord? which one?
 
-        
+
         # get transition samples, calculate TD errors, update buffer
         if self.step_count > learning_start and self.step_count % train_freq == 0:
             # minimize the error of Bellman equation on a batch sampled from replay buffer
-            if self.prioritized_replay: 
+            if self.prioritized_replay:
                 experience_x = self.replay_buffer_x.sample(batch_size, beta=self.beta_schedule_x.value(t))
                 experience_y = self.replay_buffer_y.sample(batch_size, beta=self.beta_schedule_y.value(t))
-                
+
                 (obs_t_y, actions_y, rewards_y, obs_tp1_y, dones_y, weights_y, batch_idxes_y) = experience_y
                 (obs_t_x, actions_x, rewards_x, obs_tp1_x, dones_x, weights_x, batch_idxes_x) = experience_x
 
@@ -306,13 +301,13 @@ class TerranAgent(base_agent.BaseAgent):
             td_errors_x = self.train_x(obs_t_x, actions_x, rewards_x, obs_tp1_x, dones_x, weights_x)
             td_errors_y = self.train_y(obs_t_y, actions_y, rewards_y, obs_tp1_y, dones_y, weights_y)
 
-            if self.prioritized_replay: 
+            if self.prioritized_replay:
                 new_priorities_x = np.abs(td_errors_x) + prioritized_replay_eps
                 new_priorities_y = np.abs(td_errors_x) + prioritizied_replay_eps
                 self.replay_buffer_x.update_priorities(batch_idxes_x, new_priorities_x)
                 self.replay_buffer_y.update_priorities(batch_idxes_y, new_priorities_y)
 
-        if self.step_count > learning_starts and self.step_count % target_network_update_freq == 0:  
+        if self.step_count > learning_starts and self.step_count % target_network_update_freq == 0:
             # update target networks
             self.update_target_x()
             self.update_target_y()
@@ -343,7 +338,7 @@ class TerranAgent(base_agent.BaseAgent):
 #             logger.log("Restored model with mean reward: {}".format(saved_mean_reward))
 #           U.load_state(model_file)
         self.step_count += 1
-    
+
 
 ######################################################################################
 # act: chooses an action based on observation. returns a tensor of shape (BATCH_SIZE,)
@@ -352,37 +347,42 @@ class TerranAgent(base_agent.BaseAgent):
 # (batch_size,) with differences between Q and target (TD error) for each element.
 ######################################################################################
 
+
+
 def main(unused_argv):  # could i incorporate this elsewhere?
-    map ='Blackpink'
+    agent = TerranAgent()
     try:
-        while True:  
+        while True:
             with sc2_env.SC2Env(
-                map_name=map,
+                map_name='Blackpink',
                 step_mul=8,
                 visualize=True,
                 players=[sc2_env.Agent(sc2_env.Race.terran), sc2_env.Bot(sc2_env.Race.terran, sc2_env.Difficulty.very_easy)],
                 agent_interface_format=features.AgentInterfaceFormat(
-                        feature_dimensions=features.Dimensions(screen=16, minimap=16), use_feature_units=True),
-                game_steps_per_episode=0) as env:
+                        feature_dimensions=features.Dimensions(screen=32, minimap=32), use_feature_units=True),
+                game_steps_per_episode=0
+                ) as env:
 
                 agent.setup(env.observation_spec(), env.action_spec())
-                timesteps = env.reset()  
-                agent.reset() 
+                timesteps = env.reset()
+                agent.reset()
 
                 # .reset returns TimeStep object: ['step_type', 'reward', 'discount', 'observation']
                 # agent.step takes an obs and returns action. env.step takes an action and returns TimeStep object
-                
+
                 while True:
                     step_actions = [agent.step(obs=timesteps[0], env=env)]  # step w/ obs to get action. can i do env like this?
+
                     if timesteps[0].last():
                         break
-                        EPISODE_REWARDS.append(0.0)
+
+                    EPISODE_REWARDS.append(0.0)
                     timesteps = env.step(step_actions)  # step w/ action
 
 
     except KeyboardInterrupt:
         pass
 
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run(main)
 
