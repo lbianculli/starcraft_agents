e0271e71156a024ac2426a642245823409c9a057
lbianculli <35279143+lbianculli@users.noreply.github.com>
2019-05-08 11:50:01 -0400

    Update dqn_move_only.py

--- dqn_minigames/dqn_move_only.py
+++ dqn_minigames/dqn_move_only.py
@@ -27,6 +27,7 @@ class DQNMoveOnly(base_agent.BaseAgent):
                  epsilon_decay_steps,
                  train_freq,
                  target_update_frequency,
+                 indicate_nonrandom_action,
                  max_buffer_size,
                  batch_size, 
                  training,
@@ -54,9 +55,10 @@ class DQNMoveOnly(base_agent.BaseAgent):
         self.epsilon_decay_steps = epsilon_decay_steps
         self.train_freq = train_freq
         self.target_update_freq = target_update_freq
+        self.indicate_nonrandom_action = indicate_nonrandom_action  # not sure exactly
         
         # other
-        self.training = training
+        self.training = training  # not sure exactly
         
         # build network
         self.save_path = save_dir + ckpt_name + ".ckpt"
@@ -73,6 +75,11 @@ class DQNMoveOnly(base_agent.BaseAgent):
                                                             spatial_dims=FEATURE_SCREEN_SIZE,
                                                             learning_rate=self.learning_rate,
                                                             name='target_network')
+
+            # initialize Experience Replay memory buffer
+            self.replay_buffer = ReplayBuffer(max_buffer_size)
+            self.batch_size = batch_size
+
             
         print('Initialization complete.')
         self.last_state = None
@@ -103,12 +110,36 @@ class DQNMoveOnly(base_agent.BaseAgent):
         if FUNCTIONS.Move_screen.id in obs.observation.available_actions:
             state = obs.observation.feature_screen.player_relative
             
-            if self.training:
+            if self.training:  # calls with min epsilon = None
                 # predict and take an action
-                x, y, action = self._epsilon_greedy_action_selection(state)
+                x, y, action = self._epsilon_greedy_action(state)
                 
-                # update online DQN 
-                # ...
+                # update online DQN/target network if appropriate
+                if (self.steps % self.train_freq == 0) and 
+                (len(self.replay_buffer) > self.batch_size):
+                    self._train_network()
+                    
+                if self.steps % self.update_train_freq == 0:
+                    self._update_target_networks()
+                
+                # add to replay buffer
+                if self.last_state is not None:
+                    self.replay_buffer.add(self.last_state, self.last_action, obs.reward, state)
+                
+                self.last_state = state
+                self.last_action = np.ravel_multi_index((x,y), FEATURE_SCREEN_SIZE)
+                
+            else:
+                x, y, action = self.epsilon_greedy_action(state, self.epsilon_min)
+                
+            if self.indicate_nonrandom_action and action == 'nonrandom':
+                # cosmetic difference between random and Q based actions
+                return FUNCTIONS.Attack_screen('now', (x, y))
+            
+            else:
+                return FUNCTIONS.Move_screen('now', (x,y))
+        else:
+            return FUNCTIONS.select_army('select')
 
     
     def _handle_episode_end(self):
@@ -127,19 +158,20 @@ class DQNMoveOnly(base_agent.BaseAgent):
         
         
     def _update_target_networks(self):
-        online_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'DQN') # what does this do exactly?
+        ''' get all trainable vars from graph for online and target, zip, then assign from online --> target '''
+        online_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'DQN') 
         target_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'target_network')
         
         update_op = []
         for online_var, target_var in zip(online_vars, target_vars):
-            update_op.append(target_var.assign(online_var))  # what is this ? 
+            update_op.append(target_var.assign(online_var))  
             
         self.sess.run(update_op)
         
         
     def _epsilon_greedy_action(self, state, epsilon=None):
         ''' Choose an action from the state with eps greedy *** needs updates *** '''
-        step = self.network.global_step.eval(self.sess)  #global_step +1 after the variables have been updated (opt/loss?)
+        step = self.network.global_step.eval(self.sess)  #global_step +1 after the variables have been updated 
         
         if epsilon is None:
             epsilon = max(self.epsilon_min,
@@ -152,25 +184,34 @@ class DQNMoveOnly(base_agent.BaseAgent):
             return x, y, random
         else:
             inputs = np.expand_dims(state, 0)  # state = obs (from above)
-            q_values = self.sess.run(self.network.flatten, feed_dict={self.network.inputs:inputs})  # why need to flatten?
+            q_values = self.sess.run(self.network.flatten, feed_dict={self.network.inputs:inputs})  # flatten for unravel
             best_action = np.argmax(q_values)
-            x, y = np.unravel_index(max_index, FEATURE_SCREEN_SIZE)
+            x, y = np.unravel_index(max_index, FEATURE_SCREEN_SIZE)  # not entirely sure why this is best, complicated
             return x, y, 'nonrandom'
         
         
     def _train_network(self):
+        ''' runs optimization for online network '''
+        states, actions, targets = self._get_batch()
+        self.network.optimizer_op(self.sess, states, actions, targets)
             
+            
+    def _get_batch(self):
+        ''' 
+        uses openai.baselines replay buffer to sample transitions.
+        returns batch of observations_t, actions_t and targets (rewards) 
+        '''
+        batch = self.replay_buffer.sample(self.batch_size)  # init requires size, sample requires batch_size
+        obses, actions, obses_tp1, rewards, dones = batch  # these will be arrays of len batch_size
         
-
+        # one-hot encode actions
+        actions = np.eye(np.prod(FEATURE_SCREEN_SIZE))[actions]  # how does this work?
         
+        # get targets
+        next_outputs = self.sess.run(
+            self.target_network.output(feed_dict={self.target_network.inputs: obses_tp1}))  # why tp1 over current?
+        targets = [rewards[i] + self.gamma * np.max(next_output[i]) for i in range(self.batch_size)]
         
-                
-                
-              
-            
-            
-            
-            
-            
-            
+        return obses, actions, targets
+        
 
