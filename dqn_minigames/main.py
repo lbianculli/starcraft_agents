ce4b20595f2534a0efc86ec419662c2b847b3e37
lbianculli <35279143+lbianculli@users.noreply.github.com>
2019-05-08 15:59:44 -0400

    Update main.py

--- dqn_minigames/main.py
+++ dqn_minigames/main.py
@@ -7,24 +7,47 @@ from pysc2.env import run_loop
 from pysc2.env import sc2_env
 from pysc2.lib import point_flag
 from pysc2.lib import stopwatch
+from pysc2.env import run_loop
 
-def run_thread(agent_classes, players, map_name, visualize):
-    """Run one thread worth of the environment with agents."""
-    with sc2_env.SC2Env(map_name=map_name,
-                        players=players,
-                        agent_interface_format=sc2_env.parse_agent_interface_format(
-                            feature_screen=FLAGS.feature_screen_size,
-                            feature_minimap=FLAGS.feature_minimap_size,
-                            rgb_screen=FLAGS.rgb_screen_size,
-                            rgb_minimap=FLAGS.rgb_minimap_size,
-                            action_space=FLAGS.action_space,
-                            use_feature_units=FLAGS.use_feature_units),
-                        step_mul=FLAGS.step_mul,
-                        game_steps_per_episode=FLAGS.game_steps_per_episode,
-                        disable_fog=FLAGS.disable_fog,
-                        visualize=visualize) as env:
-        env = available_actions_printer.AvailableActionsPrinter(env)
-        agents = [agent_cls() for agent_cls in agent_classes]
-        run_loop.run_loop(agents, env, FLAGS.max_agent_steps, FLAGS.max_episodes)
-        if FLAGS.save_replay:
-            env.save_replay(agent_classes[0].__name__)
+from dqn_move_only import DQNMoveOnlyAgent as DQNAgent
+
+            
+def run_loop(agents, players, map_, visualize=False):  # could i incorporate this elsewhere?
+    ''' set up and run sc2_env loop '''
+    agent = DQNAgent()
+    try:
+        while True:
+            with sc2_env.SC2Env(
+                map_name=map_,
+                step_mul=8,
+                visualize=visualize,
+                players=players,
+                agent_interface_format=features.AgentInterfaceFormat(
+                        feature_dimensions=features.Dimensions(screen=32, minimap=32), use_feature_units=True),
+                game_steps_per_episode=0
+                ) as env:
+
+                env = available_actions_printer.AvailableActionsPrinter(env)  # what this do?
+                run_loop.run_loop(agent, env, max_frames=0, max_episodes=0)
+
+                
+    except KeyboardInterrupt:
+        pass
+    
+    
+def main(unused_argv):
+    ''' run agent in env loop '''
+    stopwatch.sw.enabled = False
+    stopwatch.sw.trace = False
+    
+    map_ = 'CollectMineralShards'
+    map_inst = maps.get(map_)
+    agent = DQNAgent()
+    players=[sc2_env.Agent(sc2_env.Race.terran)]
+    
+    run_loop(agent_classes, players, map_, visualize=True)
+
+
+if __name__ == '__main__':
+    app.run(main)
+    
