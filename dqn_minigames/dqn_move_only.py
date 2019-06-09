e2c72a017911eab96f6c336d370670104d48d137
lbianculli <35279143+lbianculli@users.noreply.github.com>
2019-06-09 11:03:28 -0400

    Update dqn_move_only.py

--- dqn_minigames/dqn_move_only.py
+++ dqn_minigames/dqn_move_only.py
@@ -1,4 +1,7 @@
 """Deep Q-learning agents."""
+# TODO: dueling DQN??? LOOK AT ALL NOTES PREFIXED W/ '***', EXP DECAY FOR LEARNING RATE?
+# OTHER: activations seem to be vanishing? Is this just because epislon greedy approach?
+
 import numpy as np
 import os
 import tensorflow as tf
@@ -17,8 +20,8 @@ from dqn_utils import LinearSchedule
 from dqn_value_estimator import PlayerRelativeMovementCNN
 
 
+
 FEATURE_SCREEN_SIZE = [32,32]  # should this be [32, 32] or 32? ??? clone his tomorrow to see if it works
-FEATURE_MINIMAP_SIZE = 32  # not actually needed for minigames
 FUNCTIONS = sc2_actions.FUNCTIONS
 
 
@@ -26,31 +29,32 @@ class DQNMoveOnlyAgent(base_agent.BaseAgent):
     ''' DQN that takes in player_relative features and returns movements '''
 
     def __init__(self,
-                 learning_rate,  # could use linearschedule here as well?
-                 gamma,
-                 epsilon_max,
-                 epsilon_min,
-                 epsilon_decay_steps=20000,
-                 learning_starts=1000,
-                 train_freq=4,
+                 learning_rate=5e-4,  # could use linearschedule here as well?
+                 gamma=.99,
+                 epsilon_max=1.0,
+                 epsilon_min=0.02,
+                 epsilon_decay_steps=100000,
+                 learning_starts=2000,
+                 train_freq=10,
                  target_update_freq=5000,
-                 max_buffer_size=25000,
+                 max_buffer_size=50000,
                  batch_size=16,
-                 prioritized_replay_beta_iters = 10000,  # what is a good value for this?
+                 prioritized_replay_beta_iters = 50000,  # in reality this would be max_steps -- for now just much larger than decay steps
                  training=True,
                  indicate_nonrandom_action=False,
                  prioritized=True,
-                 prioritized_alpha = .7,  #these values are for rank-based prioritization
-                 prioritized_beta = .5,
-                 epsilon_file='/home/lbianculli/sc_bot/minigames/dqn_minigames/logs/epsilon',
+                 prioritized_alpha = .6,  # b=.7, a=.5 for rank-based prioritization
+                 prioritized_beta = .4,  # "rank-based likely not as good for sparse-reward structures" ... clipping limits outliers
+                 save_file='/home/lbianculli/sc_bot/minigames/dqn_minigames/logs/network_saves',
                  save_dir='/home/lbianculli/sc_bot/minigames/dqn_minigames/logs/ckpts/',
                  ckpt_name='collect_minerals_5-09',
                  summary_path='/home/lbianculli/sc_bot/minigames/dqn_minigames/logs/summaries/',
                  buffer_path='/home/lbianculli/sc_bot/minigames/dqn_minigames/logs/buffers/buffer_5-09',
+                 logdir='/home/lbianculli/sc_bot/minigames/dqn_minigames/logs/variable_logs/',
                  log=True):
 
         super(DQNMoveOnlyAgent, self).__init__()
-        
+
         # NN hparams
         self.learning_rate = learning_rate
         self.gamma = gamma
@@ -66,10 +70,9 @@ class DQNMoveOnlyAgent(base_agent.BaseAgent):
         self.prioritized = prioritized
         self.prioritized_alpha = prioritized_alpha
         self.prioritized_beta = prioritized_beta
-        self.epsilon_file = epsilon_file
+        self.save_file = save_file
         self.batch_size = batch_size
         self.log = log
-        self.episode_td_errors = []
 
         # other
         self.training = training  # not sure exactly
@@ -85,12 +88,14 @@ class DQNMoveOnlyAgent(base_agent.BaseAgent):
             self.buffer_file = buffer_path + '.p'
 
         # load and set epsilon
-        if os.path.isfile(self.epsilon_file + '.npy'):
-            self.epsilon = np.load(self.epsilon_file + '.npy')
+        if os.path.isfile(self.save_file + '.npy'):
+            self.epsilon, self.initial_step = np.load(self.save_file + '.npy')  # can i just use loaded step for epsilon as well?
             print(f'epsilon loaded: {self.epsilon}')
         else:
             self.epsilon = 1.0
+            self.initial_step = 0
         self.epsilons = [self.epsilon]
+
 
         # for saving and loading files
         if save_dir:
@@ -105,7 +110,7 @@ class DQNMoveOnlyAgent(base_agent.BaseAgent):
             self.target_summary_path = summary_path + 'target/'
 
         if self.log:
-            self.init_logger('/home/lbianculli/sc_bot/minigames/dqn_minigames/logs/variable_logs/')
+            self.init_logger(logdir)
 
         # build network
         if save_dir and ckpt_name:
@@ -125,6 +130,8 @@ class DQNMoveOnlyAgent(base_agent.BaseAgent):
                                                             save_path = self.target_save_path,
                                                             summary_path = self.target_summary_path,
                                                             name='target_network')
+
+        # initialize tf session
         self.sess = tf.Session()
         print('Initialization complete.')
 
@@ -139,9 +146,14 @@ class DQNMoveOnlyAgent(base_agent.BaseAgent):
                 self.replay_buffer = pickle.load(f)
         else:
             if self.prioritized:  # alpha = 0 is same as uniform
-                self.replay_buffer = PrioritizedReplayBuffer(max_buffer_size, self.prioritized_alpha)  # what about buffer methods? check baselines examples?
+                self.replay_buffer = PrioritizedReplayBuffer(max_buffer_size, self.prioritized_alpha)
             else:
                 self.replay_buffer = ReplayBuffer(max_buffer_size)
+
+        self.online_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'DQN')
+        self.target_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'target_network')
+        self.online_network._init_train_fn(self.online_vars, grad_norm_clipping=10)  # what are good values for clip?
+        self.target_network._init_train_fn(self.target_vars, grad_norm_clipping=10)
 
         print('online and target models loaded.')
         self._tf_init()
@@ -177,15 +189,17 @@ class DQNMoveOnlyAgent(base_agent.BaseAgent):
         if FUNCTIONS.Move_screen.id in obs.observation.available_actions:
             state = obs.observation.feature_screen.player_relative
 
-            if self.training:  
+
+            if self.training:  # calls with min epsilon = None
                 # predict and take an action
                 x, y, action = self._epsilon_greedy_action(state, self.epsilon)
 
                 # update online DQN/target network if appropriate
                 if (self.steps % self.train_freq == 0) and (len(self.replay_buffer) > self.batch_size):
-                    self._train_network()
+                    self._train_network()  # obviously agent_step will result in less updates...
 
-                if self.agent_step % self.target_update_freq == 0:
+
+                if self.steps % self.target_update_freq == 0:
                     self._update_target_network()
 
                 # add to replay buffer
@@ -199,7 +213,7 @@ class DQNMoveOnlyAgent(base_agent.BaseAgent):
                 x, y, action = self.epsilon_greedy_action(state, self.epsilon_min)
 
             if self.indicate_nonrandom_action and action == 'nonrandom':
-                # cosmetic difference between random and Q based actions
+                # cosmetic difference between random and Q based actions (???)
                 return FUNCTIONS.Attack_screen('now', (x, y))
 
             else:
@@ -207,9 +221,8 @@ class DQNMoveOnlyAgent(base_agent.BaseAgent):
         else:
             return FUNCTIONS.select_army('select')
 
-        
-    def init_logger(self, dir=None):
 
+    def init_logger(self, dir):
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
         file_handler = logging.FileHandler(dir, mode='w')
@@ -221,21 +234,22 @@ class DQNMoveOnlyAgent(base_agent.BaseAgent):
 
     def log_progress(self):
         # ''' log some stats and measurements in a separate file'''
-        self.logger.info(f'Game steps completed: {self.steps}')
-        self.logger.info(f'Agent steps completed: {self.agent_step}')  # same thing as global step -- + 60 for each ep
-        self.logger.info(f'Episodes completed: {self.episode+1}')
+        self.logger.info(f'Total Game steps completed: {self.total_steps}')
+        self.logger.info(f'Steps completed this run: {self.steps}')
+        self.logger.info(f'Episodes completed this run: {self.episode+1}')
         self.logger.info(f'Weight values: {self.batch_weights}')
-        self.logger.info(f'Beta value: {self.beta_schedule.value(self.agent_step)}')
+        self.logger.info(f'Beta value: {self.beta_schedule.value(self.total_steps)}')
         self.logger.info(f'Epsilon: {self.epsilons[-1]}')
 
-        
+
+
     def _handle_episode_end(self):
         ''' Save weights, eps, buffer, and write summaries '''
         self.online_network.increment_global_episode_op(self.sess)
 
         #save online and target nets and write summaries
-        if self.episodes % 5 ==  1: 
-            self._save_and_clear()
+        if self.episodes % 5 ==  1:
+            self._save()
         states, actions, targets = self._get_batch()
         self.online_network.write_summary(self.sess, states, actions, targets, self.reward)
 
@@ -249,25 +263,25 @@ class DQNMoveOnlyAgent(base_agent.BaseAgent):
         self.sess.run(init_op)
 
 
-    def _save_and_clear(self):  
-        ''' Saves models, epsilon, buffer. cleans directories (NOTIMPLEMENTED'''
+    def _save(self):
+        ''' Saves models, epsilon, buffer '''
         self.online_network.save_model(self.sess)
         self.target_network.save_model(self.sess)
 
         if self.training:
-            np.save(self.epsilon_file + '.npy', self.epsilons[-1])
+            # know how to save multiple w/ np, but what about incrementing after load? episode should be easy, but agent step...
+            np.save(self.save_file + '.npy', [self.epsilons[-1], self.total_steps])
 
         with open(self.buffer_file, 'wb') as f:
-            pickle.dump(self.replay_buffer, f)  
+            pickle.dump(self.replay_buffer, f)  # this should overwrite, so thats set
 
 
     def _update_target_network(self):
         ''' get all trainable vars from graph for online and target, zip, then assign from online --> target '''
-        online_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'DQN')
-        target_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'target_network')
-
+        # *** do vars change??? if so that would really mess this up
+        # it is setup the same in the homework so I doubt it unless they fucked it up too. Still make sure next time i have internet
         update_op = []
-        for online_var, target_var in zip(online_vars, target_vars):
+        for online_var, target_var in zip(self.online_vars, self.target_vars):
             update_op.append(target_var.assign(online_var))
 
         self.sess.run(update_op)
@@ -275,8 +289,9 @@ class DQNMoveOnlyAgent(base_agent.BaseAgent):
 
     def _epsilon_greedy_action(self, state, epsilon=1.0):
         ''' Choose an action from the state with eps greedy '''
-        self.agent_step = self.online_network.global_step.eval(self.sess)  # agent step differs from game step due to step mul. Also begins updating after opt
-        fraction = min(float(self.agent_step) / self.epsilon_decay_steps, 1.0)
+        self.total_steps = int(self.steps + self.initial_step)  # agent step differs from game step due to step mul. Also begins updating after opt
+
+        fraction = min(float(self.total_steps) / self.epsilon_decay_steps, 1.0)
         upd_epsilon = epsilon + fraction * (self.epsilon_min - self.epsilon_max)
         self.epsilons.append(upd_epsilon)  # for logging/loading
 
@@ -298,53 +313,61 @@ class DQNMoveOnlyAgent(base_agent.BaseAgent):
     def _train_network(self):
         # if self.steps >= self.learning_starts:
         states, actions, targets = self._get_batch()
-        self.online_network.optimizer_op(self.sess, states, actions, targets)
+        self.online_network.optimizer_op(self.sess, states, actions, targets, self.online_vars)
 
-
-    def _calc_ranks(self, idxes, td_errors):
+    def _calc_priorities(self, idxes, td_errors, rank=False, prioritized_epsilon=1e-5):
         '''
-        calcs ranked-based priorities given inputs.
+        calculates priorities given inputs. Can have rank-based or regular.
         returns: rank-based priorities and their corresponding indexes as lists
         '''
         #  P(i) = (pi^ a) / sum(pk^a);  p(i) = 1 / rank(i)
-        zipped = sorted(list(zip(td_errors, idxes)))
-        sorted_idxes = []
-        sorted_abs = []
-        for pair in zipped:
-            sorted_abs.append(pair[0])
-            sorted_idxes.append(pair[1])
+        if rank:
+            zipped = sorted(list(zip(td_errors, idxes)))
+            idxes = []
+            sorted_abs = []
+            for pair in zipped:
+                sorted_abs.append(np.abs(pair[0]))
+                idxes.append(pair[1])
 
-        ranks = [1/v for v in range(1, len(sorted_abs) + 1)]
+            priorities = [1/v for v in range(1, len(sorted_abs) + 1)]
 
-        return sorted_idxes, ranks
+        else:
+            priorities = np.abs(td_errors) + prioritized_epsilon
+
+        return idxes, priorities
 
 
     def _get_batch(self):
         '''
-        uses openai.baselines replay buffer to sample transitions.
+        uses [openai.baselines] replay buffer to sample transitions.
         returns batch of observations_t, actions_t and targets (rewards)
         '''
         if self.prioritized:
             # how can i log?
-            batch = self.replay_buffer.sample(self.batch_size, beta=self.beta_schedule.value(self.agent_step))
-            obses, actions, rewards, obses_tp1, dones, self.batch_weights, batch_idxes = batch 
+            batch = self.replay_buffer.sample(self.batch_size, beta=self.beta_schedule.value(self.total_steps))
+            obses, actions, rewards, obses_tp1, dones, self.batch_weights, batch_idxes = batch
 
         else:
             batch = self.replay_buffer.sample(self.batch_size)
             obses, actions, rewards, obses_tp1, dones = batch
 
-        actions = np.eye(np.prod(FEATURE_SCREEN_SIZE))[actions] 
+        actions = np.eye(np.prod(FEATURE_SCREEN_SIZE))[actions]  # how does this work?
 
         next_output = self.sess.run(self.target_network.output, feed_dict={self.target_network.inputs: obses_tp1})
         targets = [rewards[i] + self.gamma * np.max(next_output[i]) for i in range(self.batch_size)]
-        td_errors = self.sess.run([self.online_network.loss2,],
+        td_errors = self.sess.run([self.online_network.loss,],
                                 feed_dict={self.online_network.inputs: obses,
                             self.online_network.actions: actions,
                             self.online_network.targets: targets})
         td_errors = td_errors[0]
-        weighted_errors = self.batch_weights * td_errors
-        sorted_idxes, ranks = self._calc_ranks(batch_idxes, weighted_errors)
 
-        self.replay_buffer.update_priorities(sorted_idxes, ranks)
+        weighted_errors = self.batch_weights * td_errors  # "weights can be folded into Q-learning by using w*delta instead of delta"
+        idxes, new_priorities = self._calc_priorities(batch_idxes, weighted_errors, rank=False)
+        self.replay_buffer.update_priorities(idxes, new_priorities)
 
         return obses, actions, targets
+
+
+
+
+
