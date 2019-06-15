06302a4e119af03ea4c22b948e086567dca8908c
lbianculli <35279143+lbianculli@users.noreply.github.com>
2019-06-15 17:58:15 -0400

    Update main.py

--- dqn_minigames/main.py
+++ dqn_minigames/main.py
@@ -1,32 +1,34 @@
+import importlib
 import threading
-import importlib  # what exactly does this do?
 
-from absl import flags, app  # should get more familiar w/ these too
+from absl import flags, app
 
 from pysc2 import maps
 from pysc2.env import run_loop as run_loop
 from pysc2.env import sc2_env, available_actions_printer
 from pysc2.lib import stopwatch, actions, features, units, point_flag
-from a2c_agent import a2cAgent
+from dqn_move_only import DQNMoveOnlyAgent as DQNAgent
 
 
-AGENT = a2cAgent()
-BOTS = [sc2_env.Bot(sc2_env.Race.terran, sc2_env.Difficulty.very_easy)]
-PLAYERS = [sc2_env.Agent(sc2_env.Race.terran)]
-MAP = 'Simple64'
+AGENT = DQNAgent(learning_rate=5e-4,  # note, epsilon follows a schedule in DQN homework...linear schedule?
+                        batch_size=1,
+                        prioritized=True)
 
-def run_thread(agents, players, map_name, visualize=False, save_replay=False):
+
+def run_thread(agents, players, map_, visualize=False):
     ''' set up and run sc2_env loop '''
     # agents = [AGENT,]
     try:
         while True:
             with sc2_env.SC2Env(
-                map_name=map_name,
-                step_mul=16,
+                map_name=map_,
+                step_mul=8,
                 visualize=visualize,
                 players=players,
-                agent_interface_format=features.AgentInterfaceFormat(
-                    feature_dimensions=features.Dimensions(screen=32, minimap=32),
+                agent_interface_format=sc2_env.parse_agent_interface_format(
+                    feature_screen=32,
+                    feature_minimap=32,
+                    action_space=None,  # what to do about this later?
                     use_feature_units=True),
                 game_steps_per_episode=0
                 ) as env:
@@ -35,6 +37,7 @@ def run_thread(agents, players, map_name, visualize=False, save_replay=False):
                 # agents = [agent_cls() for agent_cls in agent_classes]
                 env = available_actions_printer.AvailableActionsPrinter(env)  # what this do?
                 run_loop.run_loop(agents, env, max_frames=0, max_episodes=0)
+
 
     except KeyboardInterrupt:
         pass
@@ -45,18 +48,18 @@ def main(unused_argv):
     stopwatch.sw.enabled = False
     stopwatch.sw.trace = False
 
-    map_name = 'Simple64'
-    map_ = maps.get(map_name)
-    agents = [AGENT]
-    players=[sc2_env.Agent(sc2_env.Race.terran), sc2_env.Bot(sc2_env.Race.terran, sc2_env.Difficulty.very_easy)]
+    map_ = 'CollectMineralShards'
+    map_inst = maps.get(map_)
+    agents = [AGENT,]
+
+
+    players=[sc2_env.Agent(sc2_env.Race.terran)]
 
     run_thread(agents, players, map_, visualize=False)
 
 
 if __name__ == '__main__':
     app.run(main)
-
-
 
 
 
