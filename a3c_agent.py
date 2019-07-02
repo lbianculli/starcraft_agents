7347af3baeb86ef577bcd9a722051a9b30ec697a
lbianculli <35279143+lbianculli@users.noreply.github.com>
2019-07-02 14:28:36 -0400

    Update a3c_agent.py

--- a3c_agent.py
+++ a3c_agent.py
@@ -2,11 +2,12 @@ import threading
 import tensorflow as tf
 from a2c_agent import a2cAgent
 
-# master would coordinate all agents. Model would be a2c network (?)
+# master would coordinate all agents. Model would be a2c network. handling envs seems chllenging,
+# esp with window popping up. is it possible w/o VM?
 class Master():
   def __init__(self, map_name, lr, save_path, summary_path):
     self.map_name = map_name
-    self.lr = lr
+    self.opt = tf.train.RMSPropOptimizer(lr, use_locking=True)
     self.save_path = save_path
     self.summary_path = summary_path
     
