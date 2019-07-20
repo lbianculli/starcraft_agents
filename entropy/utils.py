06668b35e095189535cb11b30ad947907cfdb19f
lbianculli <35279143+lbianculli@users.noreply.github.com>
2019-07-20 15:21:58 -0400

    Update utils.py

--- entropy/utils.py
+++ entropy/utils.py
@@ -84,3 +84,71 @@ def clean_dir(parent_path, verbose=True, excluded_paths=[]):
                     os.unlink(child_path)
                     if verbose:
                         print(f'{child_path} deleted')
+                        
+                        
+class SessionManager:
+  def __init__(self, sess=None, base_path="results/", ckpt_freq=100, training_enabled=True):
+    if not sess:  # would like to add something that chekcks if GPU or cpu and sets config accordingly
+      # also: how to set multiple options for configproto
+      # if gpu: config = tf.ConfigProto.gpu_options.allow_gpu_growth = True
+      sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+    tf.keras.backend.set_session(sess)  # do i need/what it do?
+
+    self.sess = sess
+    self.base_path = base_path
+    self.ckpt_freq = ckpt_freq
+    self.training_enabled = training_enabled
+    self.global_step = tf.train.get_or_create_global_step()
+    self.summary_writer = tf.summary.FileWriter(self.summaries_path)
+
+  def restore_or_init(self):
+    """ creates saver, loads from checkpoint if one exists. otherwise setup graph """
+    self.saver = tf.train.Saver()
+    ckpt = tf.train.latest_checkpoint(self.checkpoint_path)  # ?
+    if ckpt:
+      self.saver.restore(self.sess, ckpt)
+
+      if self.training_enabled:
+        # merge with previous summary session -- will this let global step load as well?
+        self.summary_writer.add_session_log(  # not sure what these do?
+          tf.SessionLog(status=tf.SessionLog.START), self.sess.run(self.global_step))
+
+    else:
+      self.sess.run(tf.global_variables_initializer())
+    # this call locks the computational graph into read-only state,
+    # as a safety measure against memory leaks caused by mistakingly adding new ops to it
+    self.sess.graph.finalize()
+
+  def run(self, tf_op, tf_inputs, inputs):
+    return self.sess.run(tf_op, feed_dict=dict(zip(tf_inputs, inputs)))  # pretty handy
+
+  def on_update(self, step):
+    """ saves checkpoint if appropriate """
+    if not self.ckpt_freq or not self.training_enabled or step % self.ckpt_freq:
+      return
+    self.saver.save(self.sess, self.checkpoint_path + "/.ckpt", global_step=step)
+
+  def add_summaries(self, tag, value, prefix='', step=None):
+    if not self.training_enabled:
+      return
+    summary = self.create_summary(prefix + "/" + tag, value)
+    self.summary_writer.add_summary(summary, global_step=step)
+
+  @staticmethod  # remember: staticmethod is ...
+  def create_summary(tag, value):  # not sure any of this
+    """ creates summary """
+    return tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+
+  @property # remember: property .... is used for...
+  def start_step(self):
+    if self.training_enabled:
+      return self.global_step.eval(session=self.sess)
+    return 0
+
+  @property
+  def summaries_path(self):
+    return self.base_path + "/summaries"
+
+  @property
+  def checkpoint_path(self):
+    return self.base_path + "/checkpoints"
