76786ef769c3e17c0781a9838bd35630d95e80d1
lbianculli <35279143+lbianculli@users.noreply.github.com>
2019-08-06 14:54:14 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,41 +1,106 @@
-      # minimap and screen temps
-      m_temp = np.array(state['feature_minimap'], dtype=np.float32)
-      m_temp = np.expand_dims(U.preprocess_minimap(m_temp), axis=0)
-      s_temp = np.array(state['feature_screen'], dtype=np.float32)
-      s_temp = np.expand_dims(U.preprocess_screen(s_temp), axis=0)
+""" 
+i think I am going to plan on totally copying structure if I have too much trouble with params.
+currently: batch_generator() returns everything that will be fed, and the SL file sets up the network and actual ops
+Ray: network sets up phs and network. agent handles ops. Feed for params depends on spatial/non. Agent settles which to use.
+"""
+### from network
+# ...
+self.argument_policy = dict()
+self.arguments = dict()
+for arg_type in actions.TYPES:
+  if len(arg_type.sizes) > 1:
+    if arg_type in SCREEN_TYPES:
+        units = self.screen_dimensions  # 64
+    elif arg_type in MINIMAP_TYPES:
+        units = self.minimap_dimensions  # 64
 
-      # m_temp = np.array(state['minimap'])
-      # m_temp = np.reshape(m_temp, [self.dimension, self.dimension,5])  # this is where shape error is originating
-      # s_temp = np.array(state['screen'])
-      # s_temp = np.reshape(s_temp, [self.dimension,self.dimension,10])
+    arg_policy_x = tf.layers.dense(
+        inputs=self.state_representation,
+        units=units[0],
+        activation=tf.nn.softmax)
 
-      # one-hot action_id
-      last_action = None
-      for action in state['actions']:
-        if last_action == action:
-          continue
+    arg_policy_y = tf.layers.dense(
+        inputs=self.state_representation,
+        units=units[1],
+        activation=tf.nn.softmax)
 
-        one_hot = np.zeros((1, 543)) # Not sure where 543 is from. Hardcoding for now
-        one_hot[np.arange(1), [action[0]]] = 1
+    self.argument_policy[str(arg_type) + "x"] = arg_policy_x
+    self.argument_policy[str(arg_type) + "y"] = arg_policy_y
 
-        for param in action[2]:
-          if param == [0]:  # seen this before. no_op i think
-            continue
-          minimap_output.append(m_temp)
-          screen_output.append(s_temp)
-          action_output.append(one_hot[0])
-          player_info_output.append(pi_temp)
-          ground_truth_coordinates.append(np.array(param))
+    arg_placeholder_x = tf.placeholder(
+        tf.float32,
+        shape=[None, units[0]])
 
-    assert(len(minimap_output) == len(ground_truth_coordinates))
+    arg_placeholder_y = tf.placeholder(
+        tf.float32,
+        shape=[None, units[1]])
 
-    if len(minimap_output) == 0:
-      # The replay file only record one person's operation, so if it is
-      # the defeated person, we need to skip the replay file
-      return self.next_batch(get_action_id_only)
+    self.arguments[str(arg_type) + "x"] = arg_placeholder_x
+    self.arguments[str(arg_type) + "y"] = arg_placeholder_y
 
-    if get_action_id_only:
-      return minimap_output, screen_output, player_info_output, action_output
-    else:
-      return minimap_output, screen_output, action_output, player_info_output, ground_truth_coordinates
+else:
+    arg_policy = tf.layers.dense(
+        inputs=self.state_representation,
+        units=arg_type.sizes[0],
+        activation=tf.nn.softmax)
 
+    self.argument_policy[str(arg_type)] = arg_policy
+
+    arg_placeholder = tf.placeholder(
+        tf.float32,
+        shape=[None, arg_type.sizes[0]])
+
+    self.arguments[str(arg_type)] = arg_placeholder
+
+-----------------------------------------------------
+### from agent
+# def get_batch(self, terminal) ...
+        feed_dict = {self.network.screen_features: screen,
+                     self.network.minimap_features: minimap,
+                     self.network.flat_features: flat,
+                     self.network.actions: actions,
+                     self.network.returns: returns}  # essentially same, but dont have returns now
+
+        # add args and arg_types to feed_dict
+        net_args = self.network.arguments
+        batch_size = len(arg_types)
+
+        # first populate feed_dict with zero arrays
+        for arg_type in sc2_actions.TYPES:
+          if len(arg_type.sizes) > 1:
+            if arg_type in SCREEN_TYPES:
+                x_size = feature_screen_size[0]  # all these are 64
+                y_size = feature_screen_size[1]
+            elif arg_type in MINIMAP_TYPES:
+                x_size = feature_minimap_size[0]
+                y_size = feature_minimap_size[1]
+
+            feed_dict[net_args[str(arg_type) + "x"]] = np.zeros(  # feed_dict["ph_name"]["arg_typex"]
+                (batch_size, x_size))
+            feed_dict[net_args[str(arg_type) + "y"]] = np.zeros(
+                (batch_size, y_size))
+
+            else:
+              feed_dict[net_args[str(arg_type)]] = np.zeros(
+                  (batch_size, arg_type.sizes[0]))
+
+        # then one_hot encode args
+        for step in range(batch_size):
+          for i, arg_type in enumerate(arg_types[step]):
+            if len(arg_type.sizes) > 1:
+              arg_key_x = net_args[str(arg_type) + "x"]
+              feed_dict[arg_key_x][step, args[step][i][0]] = 1
+
+              arg_key_y = net_args[str(arg_type) + "x"]
+              feed_dict[arg_key_y][step, args[step][i][1]] = 1
+            else:
+              arg_key = net_args[str(arg_type)]
+              feed_dict[arg_key][step, args[step][i][0]] = 1
+                    
+        return feed_dict
+      
+      
+def _train_network(self, terminal=False):
+    feed_dict = self._get_batch(terminal)
+    self.network.optimizer_op(self.sess, feed_dict)
+    return feed_dict
