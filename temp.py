4600539891dc3da6b5eb1d20bda16004e5476c1c
lbianculli <35279143+lbianculli@users.noreply.github.com>
2019-08-10 09:13:14 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,102 +1,118 @@
-import tensorflow as tf
+import gin.tf
 import numpy as np
-from batch_generator import BatchGenerator
-import logging
-import utils as U
-from pysc2.lib import actions
-import tensorflow.contrib.layers as layers
+import tensorflow as tf
+from abc import abstractmethod
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler("C:/Users/lbianculli/action_param_log", mode="w")
-file_handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
-isize = 11
-msize= 64
-ssize = 64
-
-score = tf.placeholder(tf.int32, [], name='score')
-minimap = tf.placeholder(tf.float32, [None, U.minimap_channel(), msize, msize], name='minimap')  # 17, 64, 64
-screen = tf.placeholder(tf.float32, [None, U.screen_channel(), ssize, ssize], name='screen')
-info = tf.placeholder(tf.float32, [None, isize], name='info')
-param_output = tf.placeholder(tf.float32, [None, 2], name="params")
-
-# minimap_placeholder = tf.placeholder(tf.float32, [None, 64, 64, 5])
-# screen_placeholder = tf.placeholder(tf.float32, [None, 64, 64, 10])
-# user_info_placeholder = tf.placeholder(tf.float32, [None, isize])
-action_output = tf.placeholder(tf.float32, [None, 543]) # one hot
-
-# set up network
-screen_filters1 = tf.get_variable(name='sf1', shape=(5, 5, U.screen_channel(), 16))  # hwio
-screen_filters2 = tf.get_variable(name='sf2',shape=(3, 3, 16, 32))
-minimap_filters1 = tf.get_variable(name='mmf1',shape=(5, 5, U.minimap_channel(), 16))
-minimap_filters2 = tf.get_variable(name='mmf2',shape=(3, 3, 16, 32))
-
-mconv1 = tf.nn.conv2d(tf.transpose(minimap, [0, 2, 3, 1]), minimap_filters1, strides=[1, 1, 1, 1], padding='SAME', name='mconv1')
-mconv2 = tf.nn.conv2d(mconv1, minimap_filters2, strides=[1, 1, 1, 1], padding='SAME', name='mconv2')
-sconv1 = tf.nn.conv2d(tf.transpose(screen, [0, 2, 3, 1]), screen_filters1, strides=[1, 1, 1, 1], padding='SAME', name='sconv1')
-sconv2 = tf.nn.conv2d(sconv1, screen_filters2, strides=[1, 1, 1, 1], padding='SAME', name='sconv2')
-info_fc = layers.fully_connected(layers.flatten(info), num_outputs=256, activation_fn=tf.tanh, scope='info_fc')
-
-flat_screen = tf.reshape(sconv2, [-1, 16*16*32])       
-dense_screen = tf.layers.dense(inputs=flat_screen, units=1024, activation=tf.nn.relu)
-screen_output = tf.layers.dense(dense_screen, 256)
-
-flat_minimap = tf.reshape(mconv2, [-1, 16*16*32])   
-dense_minimap = tf.layers.dense(inputs=flat_minimap, units=1024, activation=tf.nn.leaky_relu)
-minimap_output = tf.layers.dense(dense_minimap, 64)
-
-# user info
-l1_user_info = tf.layers.dense(info, isize, tf.tanh)
-user_info_output = tf.layers.dense(l1_user_info, 5)
-
-# regression, NOT SURE IF THIS IS suitable regression
-input_to_classification = tf.concat([minimap_output, screen_output, user_info_output], 1)
-
-l2_classification = tf.layers.dense(input_to_classification, 1024, tf.nn.relu)
-classification_output = tf.layers.dense(l2_classification, 543)              # output layer
-loss = tf.losses.softmax_cross_entropy(onehot_labels=action_output, logits=classification_output)
-
-train_op = tf.train.AdamOptimizer(0.001).minimize(loss)
-tf.summary.scalar('loss', loss) # add loss to scalar summary
-
-accuracy = tf.metrics.accuracy(          # return (acc, update_op), and create 2 local variables
-    labels=tf.argmax(action_output, axis=1), predictions=tf.argmax(classification_output, axis=1),)[1]
-
-# setup for graph
-config = tf.ConfigProto(allow_soft_placement=True)
-config.gpu_options.allow_growth = True   
-sess = tf.Session(config=config)                                # control training and others
-# sess.run(tf.global_variables_initializer(), tf.local_variables_initializer())    # initialize var in graph
-init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer()) # the local var is for accuracy_op
-sess.run(init_op) 
-
-saver = tf.train.Saver() # define a saver for saving and restoring
-writer = tf.summary.FileWriter('./action_param_log', sess.graph)     # write to file
-merge_op = tf.summary.merge_all() # operation to merge all summary
+from reaver.envs.base import Spec
+from reaver.agents.base import MemoryAgent
+from reaver.utils import Logger  # how does this look?
+from reaver.utils.tensorflow import SessionManager
+from reaver.utils.typing import ModelBuilder, PolicyType
 
 
-bg = BatchGenerator()
-for step in range(1000):                             # train. does number matter?
-    m,s,a,u,y =  bg.next_batch()
-    logger.info(f"Y: {y[:20]}\n--------------------------------------\n")  # all length 1732. all lists of arrays
-    logger.info(f"screen: {s[:10]}\n--------------------------------------\n")
-    logger.info(f"action: {a[:10]}\n--------------------------------------\n")
-    _, loss_, result = sess.run([train_op, loss, merge_op],
-        {minimap: m, 
-        screen: s, 
-        action_output: a, 
-        info:u, 
-        param_output:y})  # y is issue, ground_truth_coordinates. I think the issue is some returning arrays of len(1). Non-spatial?
-    writer.add_summary(result, step)
-    print('step: ', step, 'loss: ',loss_, 'result: ', result)
-
-saver.save(sess, './params', write_meta_graph=False)  # meta_graph is not recommended
-
-
+DEFAULTS = dict(  # will have to change a lot of this later
+    model_fn=None,
+    policy_cls=None,
+    optimizer=None,
+    learning_rate=5e-4,
+    value_coef=0.5,
+    entropy_coef=0.01,
+    traj_len=16,
+    batch_sz=16,
+    gamma=0.99,
+    gae_lambda=0.95,
+    clip_rewards=0.0,
+    clip_grads_norm=0.0,  # what are good values for these?
+    normalize_returns=False,
+    normalize_advantages=False,
+)
 
 
+@gin.configurable('ACAgent')
+class ActorCriticAgent(MemoryAgent):
+  """
+  Abstract class, unifies deep actor critic functionality
+  Handles on_step callbacks, either updating current batch
+  or executing one training step if the batch is ready
+
+  Extending classes only need to implement loss_fn method
+  """
+  def __init__(
+    self,
+    obs_spec: Spec,  # how does this just work like that?
+    act_spec: Spec,
+    model_fn: ModelBuilder=None,
+    policy_cls: PolicyType=None,
+    sess_mgr: SessionManager=None,
+    optimizer: tf.train.Optimizer=None,
+    value_coef=DEFAULTS['value_coef'],
+    entropy_coef=DEFAULTS['entropy_coef'],
+    traj_len=DEFAULTS['traj_len'],
+    batch_sz=DEFAULTS['batch_sz'],
+    gamma=DEFAULTS['gamma'],
+    gae_lambda=DEFAULTS['gae_lambda'],
+    clip_rewards=DEFAULTS['clip_rewards'],
+    clip_grads_norm=DEFAULTS['clip_grads_norm'],
+    normalize_returns=DEFAULTS['normalize_returns'],
+    normalize_advantages=DEFAULTS['normalize_advantages']):
+    MemoryAgent.__init__(self, obs_spec, act_spec, traj_len, batch_sz)
+
+    if not sess_mgr:
+      sess_mgr = SessionManager()
+    if not optimizer:
+      optimizer = tf.train.AdamOptimizer(DEFAULTS["learning_rate"])
+
+    self.sess_mgr = sess_mgr
+    self.value_coef = value_coef
+    self.entropy_coef = entropy_coef
+    self.traj_len = traj_len
+    self.gamma = gamma
+    self.gae_lambda = gae_lambda
+    self.clip_rewards = clip_rewards
+    self.normalize_returns = normalize_returns
+    self.normalize_advantages = normalize_advantages
+
+    self.model = model_fn(obs_sec, act_spec)  # this is where the model build (fully conv) would go, whats obs/act_spec though?
+    self.value = self.model.outputs[-1]  # very cool
+    self.policy = policy_cls(act_spec, self.model.outputs[:-1])  # whats advantage of doing it like this?
+    self.loss_op, self.loss_terms, self.loss_inputs = self.loss_fn()  # does this have to change?
+
+    grads, vars = zip(*optimizer.compute_gradients(self.loss_op))
+    self.grads_norm = tf.global_norm(grads)
+    if clip_grads_norm > 0.:  # currently defaults at 0.
+      grads, _  = tf.clip_by_global_norm(grads, clip_grads_norm, self.grads_norm)
+    self.train_op = optimizer.apply_gradients(zip(grads, vars), global_step=sess_mgr.global_step)
+    self.minimize_ops = self.make_minimize_ops()  # whats benefit of doing like this instead of just setting them in the method?
+
+    sess_mgr.restore_or_init()
+    self.n_batches = sess_mgr.start_step
+    self.start_step = sess_mgr.start_step * traj_len  # why is there no self on these?
+    self.logger = Logger()
+
+
+  def get_action_and_value(self, obs):
+    return self.sess_mgr.run([self.policy.sample, self.value], self.model.inputs, obs)
+
+  def get_action(self, obs):
+
+
+  def on_step(self, step, obs, action, reward, done, value=None):
+
+
+  def minimize(self, advantages, returns):
+
+
+  def compute_advantages_and_returns(self, bootstrap_value):
+
+
+
+  def on_start(self):
+
+
+
+  def on_finish(self):
+
+
+
+  def make_minimize_ops(self):
 
